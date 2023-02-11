5f7926e0cd2c9645109734645c7e223753dc5063
Alisher <93440110+AlisherSrc@users.noreply.github.com>
2023-02-11 11:34:45 +0600

    almsot complete design and functionality

--- lab3/toDoApp/script.js
+++ lab3/toDoApp/script.js
@@ -3,9 +3,20 @@ const alertHello = () =>{
 }
 
 const createTask = () =>{
-    let taskName = document.getElementById("inputText").nodeValue;
-    let listItem = document.createElement('li');
-    listItem.appendChild(document.createTextNode(taskName));
+    let taskName = document.getElementById("inputText").value;
+    console.log(taskName);
+    let listItem = document.createElement('div'); // createing div itself
+    listItem.classList.add("list-item");
+
+    let textInput = document.createElement('input'); //creating input
+    textInput.setAttribute("type","checkbox");
+
+    let li = document.createElement('li');
+    li.appendChild(document.createTextNode(taskName));
+
+    let img = document.createElement('img');
+    img.classList.add("trash-icon");
 
     document.getElementById('taskList').appendChild(listItem);
 }
+
