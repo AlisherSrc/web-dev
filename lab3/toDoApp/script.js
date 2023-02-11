8ac3fa90779e40465e6b025512bf111e0ffa043b
Alisher <93440110+AlisherSrc@users.noreply.github.com>
2023-02-11 16:14:07 +0600

    deletion of element function is done

--- lab3/toDoApp/script.js
+++ lab3/toDoApp/script.js
@@ -1,10 +1,16 @@
+var cnt = 0;
+
+const getUniqueId = () =>{
+    cnt++;
+    return cnt;
+}
+
 const alertHello = () =>{
     alert("Hello");
 }
 
 const createTask = () =>{
     let taskName = document.getElementById("input-text").value; // obtaining text value
-    console.log(taskName);
 
     let li = document.createElement('li'); // creating list itself
 
@@ -13,30 +19,32 @@ const createTask = () =>{
     let checkbox = document.createElement('input'); //creating input for checkbox
     checkbox.setAttribute("type","checkbox"); // setting attribute to make it chackbox
 
-    let a = document.createElement('a');
-
-
     let img = document.createElement('img'); // creating img
-    img.classList.add("trash-icon");
+    img.classList.add("trash-icon"); 
     img.setAttribute("src","https://cdn-icons-png.flaticon.com/512/1869/1869668.png");
+
+
+    let button = document.createElement('button');
+    button.classList.add("trash");
+    button.setAttribute("onclick","deleteTask()");
+    button.appendChild(img);
 
     li.appendChild(checkbox);
     li.appendChild(document.createTextNode(taskName));
-    li.appendChild(img);
+    li.appendChild(button);
+
+    li.id = getUniqueId(); // setting id to it it order to be able to delte this task
 
     document.getElementById('task-list').appendChild(li);
-}
-
-const findTask = () => {
-
+    
 }
 
 const deleteTask = () => {
     list = document.getElementById('task-list').children;
-    
-    for(const child of list){
-        console.log(child);
-    }
 
-    document.activeElement.id
+    for(const child of list){
+        if(child.id == document.activeElement.parentNode.id){
+            child.remove();
+        }
+    }
 }
