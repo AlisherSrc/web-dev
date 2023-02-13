578a26c23bf331be9ca3ff73021e4c3c7f79c6e8
Alisher <93440110+AlisherSrc@users.noreply.github.com>
2023-02-13 10:49:49 +0600

    beatify code itself and comment example item

--- lab3/toDoApp/script.js
+++ lab3/toDoApp/script.js
@@ -2,20 +2,20 @@
 
 var cnt = 0;
 
-const getUniqueId = () =>{
+const getUniqueId = () => {
     cnt++;
     return cnt;
 }
 
-const alertHello = () =>{
+const alertHello = () => {
     alert("Hello");
 }
 
-const createTask = () =>{
+const createTask = () => {
     let taskName = document.getElementById("input-text").value; // obtaining text value
 
     console.log(taskName);
-    if (taskName.trim().length === 0){
+    if (taskName.trim().length === 0) {
         return;
     }
 
@@ -24,17 +24,17 @@ const createTask = () =>{
     li.classList.add("list-item"); //setting class to the list item
 
     let checkbox = document.createElement('input'); //creating input for checkbox
-    checkbox.setAttribute("type","checkbox"); // setting attribute to make it chackbox
+    checkbox.setAttribute("type", "checkbox"); // setting attribute to make it chackbox
     checkbox.setAttribute("onclick", "editStyle()");
 
     let img = document.createElement('img'); // creating img
-    img.classList.add("trash-icon"); 
-    img.setAttribute("src","https://cdn-icons-png.flaticon.com/512/1869/1869668.png");
+    img.classList.add("trash-icon");
+    img.setAttribute("src", "https://cdn-icons-png.flaticon.com/512/1869/1869668.png");
 
 
     let button = document.createElement('button');
     button.classList.add("trash");
-    button.setAttribute("onclick","deleteTask()");
+    button.setAttribute("onclick", "deleteTask()");
     button.appendChild(img);
 
     li.appendChild(checkbox);
@@ -44,20 +44,20 @@ const createTask = () =>{
     li.id = getUniqueId(); // setting id to it it order to be able to delte this task
 
     document.getElementById('task-list').appendChild(li);
-    
+
 }
 
 const deleteTask = () => {
     list = document.getElementById('task-list').children;
 
 
-    for(const child of list){
-        if(child.id == document.activeElement.parentNode.id){
+    for (const child of list) {
+        if (child.id == document.activeElement.parentNode.id) {
             child.style.animation = "shrink 110ms linear 0s 1";
-            
+
             setTimeout(() => {
                 child.remove();
-            },100);
+            }, 100);
 
         }
     }
@@ -65,9 +65,9 @@ const deleteTask = () => {
 
 const editStyle = () => {
     let parent = document.activeElement.parentNode;
-    if(document.activeElement.checked){
+    if (document.activeElement.checked) {
         parent.style.backgroundColor = "green";
-    }else{
+    } else {
         parent.style.backgroundColor = "white";
     }
 }
