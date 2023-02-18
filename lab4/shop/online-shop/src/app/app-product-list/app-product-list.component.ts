669565a973b6edf16042813bd414c62ee9983db0
Alisher <93440110+AlisherSrc@users.noreply.github.com>
2023-02-18 21:23:06 +0600

    add css and overall styling

--- lab4/shop/online-shop/src/app/app-product-list/app-product-list.component.ts
+++ lab4/shop/online-shop/src/app/app-product-list/app-product-list.component.ts
@@ -1,31 +1,38 @@
 import { Component } from "@angular/core";
 // import {Item} from "./item-module.module";
 
-interface Item{
-  name : string,
-  image : string,
-  description : string,
-  cost:number,
-  rating : number
+interface Item {
+  id: number,
+  name: string,
+  description: string,
+  cost: number,
+  rating: number,
+  images: string[],
+  imagesNumber: number,
 }
 
 
 @Component(
   {
     selector: "app-products",
-    templateUrl:"./app-product-list.component.html",
+    templateUrl: "./app-product-list.component.html",
     styleUrls: ["./app-product-list.component.css"]
   }
 )
 
-export class AppProductListComponent{
-  itemsList : Item[] = [
+export class AppProductListComponent {
+  itemsList: Item[] = [
     {
-      name:"Смартфон Samsung Galaxy A13 4 ГБ/128 ГБ черный",
-      cost:87411,
-      image:"link1",
-      description:"Технология NFC: Цвет: черный Тип экрана: PLS TFT LCD сенсорный, мультитач Диагональ: 6.6 дюйм Размер оперативной памяти: 4 ГБ Процессор: 8-ядерный Exynos 850 Объем встроенной памяти: 128 ГБ Емкость аккумулятора: 5000 мАч",
-      rating:5
+      id: 1,
+      name: "Смартфон Samsung Galaxy A13 4 ГБ/128 ГБ черный",
+      cost: 87411,
+      imagesNumber: 1,
+      images: ["https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h89/h78/49613712195614/samsung-galaxy-a13-4-gb-128-gb-chernyi-104253279-"], //{number of the image}.jpg
+      description: "Технология NFC\n Цвет: черный \nТип экрана: PLS TFT LCD сенсорный, мультитач \nДиагональ: 6.6 дюйм \nРазмер оперативной памяти: 4 ГБ \nПроцессор: 8-ядерный Exynos 850 \nОбъем встроенной памяти: 128 ГБ Емкость аккумулятора: 5000 мАч",
+      rating: 5
     }
   ]
+
+  stars:number[] = [1,2,3,4,5]
+
 }
