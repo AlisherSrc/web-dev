b2547e35803f5e8094da00bb19567ed89fe1ff8b
Alisher <93440110+AlisherSrc@users.noreply.github.com>
2023-03-03 20:47:40 +0600

    just styling home page

--- lab6/album/src/app/home/home.component.ts
+++ lab6/album/src/app/home/home.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  scrollImages : string[];
 
+  constructor(){
+    this.scrollImages = [
+      "https://cdn.pixabay.com/photo/2016/04/27/20/38/girl-1357485_1280.jpg",
+      "https://cdn.pixabay.com/photo/2016/11/23/00/57/adult-1851571_1280.jpg",
+      "https://cdn.pixabay.com/photo/2021/07/18/14/59/family-6475821_1280.jpg",
+      "https://cdn.pixabay.com/photo/2023/02/14/22/12/birds-7790506_1280.jpg",
+      "https://cdn.pixabay.com/photo/2013/07/18/20/26/sea-164989_1280.jpg"
+    ]
+  }
 }
