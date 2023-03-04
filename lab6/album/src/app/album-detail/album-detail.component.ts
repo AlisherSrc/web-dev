b7b4637b4eef14f7b12bb1e718234dc2da1f55b7
Alisher <93440110+AlisherSrc@users.noreply.github.com>
2023-03-04 12:19:17 +0600

    add albumDetails photos interface loaded and loading functionality

--- lab6/album/src/app/album-detail/album-detail.component.ts
+++ lab6/album/src/app/album-detail/album-detail.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AlbumsService } from '../albums.service';
+import { Album } from '../models/Post';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-album-detail',
   templateUrl: './album-detail.component.html',
   styleUrls: ['./album-detail.component.css']
 })
-export class AlbumDetailComponent {
+export class AlbumDetailComponent implements OnInit {
+  album: Album;
+  loaded: boolean;
 
+  constructor(private albumService: AlbumsService,
+              private route: ActivatedRoute) {
+    this.album = {} as Album;
+    this.loaded = true;
+  }
+
+  ngOnInit(): void {
+    this.getAlbum();
+  }
+
+  getAlbum() {
+    // We are getting parameters of the url
+    this.route.paramMap.subscribe((params) => {
+      const id = Number(params.get('id'));
+
+      this.loaded = false;
+      this.albumService.getAlbum(id).subscribe((album)=>{
+        this.album = album;
+        this.loaded = true;
+      })
+    })
+  }
 }
--- lab6/album/src/app/albums.service.ts
+++ lab6/album/src/app/albums.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Album } from './models/Post';
+import { Photo } from './models/Photo';
 
 @Injectable({
   providedIn: 'root'
@@ -21,8 +22,12 @@ export class AlbumsService {
     return this.client.get<Album[]>(`${this.BASE_URL}/albums`)
   }
 
-  getAlbum(id:number):Observable<Album[]>{
-    return this.client.get<Album[]>(`${this.BASE_URL}/albums/${id}`);
+  getAlbum(id:number):Observable<Album>{
+    return this.client.get<Album>(`${this.BASE_URL}/albums/${id}`);
+  }
+
+  getAlbumPhotos(id:number):Observable<Photo[]>{
+    return this.client.get<Photo[]>(`${this.BASE_URL}/albums/${id}/photos`);
   }
 
   postAlbum(album:Album):Observable<Album>{
